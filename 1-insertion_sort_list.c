37bf631883992c2fc6f3b83988d7d0a64312b43e
Habeeb Hassan Dindi <habeebullahdindi@gmail.com>
2022-11-16 12:21:35 +0000

    correction

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,7 +9,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *current = *list;
 
-	if (list == NULL || (*list)->next == NULL)
+	if (*list == NULL || (*list)->next == NULL)
 		return;
 	while (current->next != NULL)
 	{
