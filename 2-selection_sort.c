60d8e30404a513bc67c7fb90d4d0c97896affce9
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-06-28 01:53:25 +0000

    Add an if condition

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -17,9 +17,12 @@ void selection_sort(int *array, size_t size)
 			if (array[j] < array[min_idx])
 				min_idx = j;
 		}
-		temp = array[i];
-		array[i] = array[min_idx];
-		array[min_idx] = temp;
-		print_array(array, size);
+		if (min_idx != i)
+		{
+			temp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = temp;
+			print_array(array, size);
+		}
 	}
 }
