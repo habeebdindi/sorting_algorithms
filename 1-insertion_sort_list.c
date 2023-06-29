61c7644eabf7abdad7a7dd9af349cb463bb7efad
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-06-29 11:19:50 +0000

    Add shorter version of swap, update header along

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,7 +8,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *curr, *inner_curr;
 
-	if (*list == NULL || (*list)->next == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
 	curr = (*list)->next;
@@ -28,40 +28,19 @@ void insertion_sort_list(listint_t **list)
 /**
  * swap_nodes - swaps nodes of a @list.
  * @list: the list.
- * @node1: first node.
- * @node2: second node.
+ * @y: first node.
+ * @x: second node.
  */
-void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
+void swap_nodes(listint_t **list, listint_t *y, listint_t *x)
 {
-	listint_t *prev1, *prev2, *next1, *next2;
-
-	prev1 = node1->prev;
-	prev2 = node2->prev;
-
-	/* Update next pointers */
-	if (prev1 != NULL)
-		prev1->next = node2;
+	if (y->prev != NULL)
+		y->prev->next = x;
 	else
-		*list = node2;
-
-	if (prev2 != NULL)
-		prev2->next = node1;
-	else
-		*list = node1;
-
-	next1 = node1->next;
-	next2 = node2->next;
-
-	node1->prev = prev2;
-	node1->next = next2;
-
-	node2->prev = prev1;
-	node2->next = next1;
-
-	/* Update prev pointers */
-	if (next1 != NULL)
-		next1->prev = node2;
-
-	if (next2 != NULL)
-		next2->prev = node1;
+		*list = x;
+	if (x->next != NULL)
+		x->next->prev = y;
+	y->next = x->next;
+	x->prev = y->prev;
+	x->next = y;
+	y->prev = x;
 }
--- sort.h
+++ sort.h
@@ -23,6 +23,6 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void sorted_insert(listint_t **head, listint_t *new);
 void selection_sort(int *array, size_t size);
-void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2);
+void swap_nodes(listint_t **list, listint_t *y, listint_t *x);
 
 #endif /* SORT_H */
